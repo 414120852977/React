a4319c1bcf689031e5a18f205fe593e8f4062517
Ashok Mane <ashokmane81800@gmail.com>
2021-06-29 20:22:37 +0530

    [ashok] added clicking on logo launch bridgelabz website

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './App.css';
 import logo from './bridgLogo.png'
 
 class App extends React.Component {
+  url = 'https://www.bridgelabz.com'
   constructor() {
     super()
     this.state = {
       title:"Hello From Bridgelabz"
     }
   }
+  // onclick function
+  onClick = ($event) => {
+    console.log("Save button is clicked!!", $event);
+    window.open(this.url,"_blank");
+  }
+
   render() {
   return (
       <div>
       <h1>{this.state.title}</h1>
-      <img src = {logo}
+      <img src = {logo} onClick = {this.onClick}
       alt="The Bridgelabz Logo a bridgelabz through employement through lab works"/>
       </div>
   
