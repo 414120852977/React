bd0adbca1338b31eeab11640f6e6cfcc314b6de8
Ashok Mane <ashokmane81800@gmail.com>
2021-07-01 09:25:19 +0530

    [ashok] added home another home component to show hello world from bridgelabz

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './component/home/home';
+import Homes from './components/home/homes';
 
 // import react router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -13,6 +14,7 @@ class App extends React.Component {
       <Switch>
         <Route path = "">
         <Home/>
+        <Homes/>
         </Route>
       </Switch>
     </Router>
---
+++ src/components/home/homes.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './homes.css';
+
+class Homes extends React.Component {
+    render() {
+        return (
+                <h1>Hello World From Bridgelabz</h1>
+        );
+    }
+}
+
+export default Homes;
